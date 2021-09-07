bb624c42e83ccba39b37c135737e0bb357f29e1c
mohammadalbasha <73054919+mohammadalbasha@users.noreply.github.com>
2021-09-07 14:09:47 +0300

    Delete dashBord/dashBord directory

--- dashBord/dashBord/src/redux/root.js
+++
@@ -1,6 +0,0 @@
-// import hotelsReducer from './hotels/hotelsReducer'
-// import {combineReducers } from 'redux'
-// const root = combineReducers({
-//     hotels:hotelsReducer
-// })
-// export default root
--- dashBord/dashBord/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import root from './root'
-import {applyMiddleware, createStore} from  'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger'
-import {hotelsReducer} from './hotels/hotelsReducer'
-const store =createStore(hotelsReducer,composeWithDevTools(applyMiddleware(logger,thunk)))
-export default store
